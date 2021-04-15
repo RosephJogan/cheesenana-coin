0bf4b0fe63b10f0fccd722f0677ed0a99e37b3c9
unknown <Roe Jogan>
2021-04-15 01:22:19 -0400

    pog

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index.js')
+const registerRouter = require('./routes/register.js')
+const loginRouter = require('./routes/login.js')
 
 app.set('view-engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -10,6 +16,15 @@ app.set('layout', 'layouts/layout.ejs')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('connected to mongoose epic gamer moment'))
+
 app.use('/', indexRouter)
+app.use('/register', registerRouter)
+app.use('/login', loginRouter)
 
 app.listen(process.env.PORT || 3000)
